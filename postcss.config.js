b507b5ba3421d852f130216f8cdba028eb6023ae
Stanislav Drehval <stasdre@gmail.com>
2020-11-09 11:45:52 +0200

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,82 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-normalize': {},
+    'postcss-utilities': {},
+    'postcss-preset-env': {
+      stage: 0,
+      preserve: false,
+    },
+    'postcss-font-magician': {
+      custom: {
+        ibmplexsans: {
+          variants: {
+            normal: {
+              100: {
+                url: {
+                  woff2: '../../assets/fonts/ibmplexsans-thin-webfont.woff2',
+                  woff: '../../assets/fonts/ibmplexsans-thin-webfont.woff',
+                },
+              },
+              300: {
+                url: {
+                  woff2: '../../assets/fonts/ibmplexsans-light-webfont.woff2',
+                  woff: '../../assets/fonts/ibmplexsans-light-webfont.woff',
+                },
+              },
+              400: {
+                url: {
+                  woff2: '../../assets/fonts/ibmplexsans-regular-webfont.woff2',
+                  woff: '../../assets/fonts/ibmplexsans-regular-webfont.woff',
+                },
+              },
+              500: {
+                url: {
+                  woff2: '../../assets/fonts/ibmplexsans-medium-webfont.woff2',
+                  woff: '../../assets/fonts/ibmplexsans-medium-webfont.woff',
+                },
+              },
+              700: {
+                url: {
+                  woff2: '../../assets/fonts/ibmplexsans-bold-webfont.woff2',
+                  woff: '../../assets/fonts/ibmplexsans-bold-webfont.woff',
+                },
+              },
+            },
+            italic: {
+              100: {
+                url: {
+                  woff2: '../../assets/fonts/ibmplexsans-thinitalic-webfont.woff2',
+                  woff: '../../assets/fonts/ibmplexsans-thinitalic-webfont.woff',
+                },
+              },
+              300: {
+                url: {
+                  woff2: '../../assets/fonts/ibmplexsans-lightitalic-webfont.woff2',
+                  woff: '../../assets/fonts/ibmplexsans-lightitalic-webfont.woff',
+                },
+              },
+              500: {
+                url: {
+                  woff2: '../../assets/fonts/ibmplexsans-mediumitalic-webfont.woff2',
+                  woff: '../../assets/fonts/ibmplexsans-mediumitalic-webfont.woff',
+                },
+              },
+              700: {
+                url: {
+                  woff2: '../../assets/fonts/ibmplexsans-bolditalic-webfont.woff2',
+                  woff: '../../assets/fonts/ibmplexsans-bolditalic-webfont.woff',
+                },
+              },
+            },
+          },
+        },
+      },
+      display: 'swap',
+    },
+    autoprefixer: {},
+    cssnano: {
+      preset: 'default',
+    },
+  },
+};
