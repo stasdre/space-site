8ac064f9aa4f7f0e5548e6eb60efcc44c12d9ccf
Stanislav Drehval <stasdre@gmail.com>
2020-11-09 15:21:14 +0200

    Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import styles from './Logo.module.css';
+import LogoSvg from '../../assets/img/logo.svg';
+
+const Logo = () => (
+  <div className={styles.logo}>
+    <div className={styles.logo__svg}>
+      <LogoSvg />
+    </div>
+    <span className={styles.logo__name}>Space-site</span>
+  </div>
+);
+
+export default Logo;
