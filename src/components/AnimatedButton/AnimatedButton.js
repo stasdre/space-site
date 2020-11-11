1aa0b183d68db57be2f6ee303439c226c16e282b
Stanislav Drehval <stasdre@gmail.com>
2020-11-11 15:09:21 +0200

    AnimatedButton add grey color theme

--- src/components/AnimatedButton/AnimatedButton.js
+++ src/components/AnimatedButton/AnimatedButton.js
@@ -1,5 +1,16 @@
 import styles from './AnimatedButton.module.css';
 
-const AnimatedButton = ({ children }) => <button className={styles.button}>{children}</button>;
+const AnimatedButton = ({ children, color = 'primary' }) => {
+  let themeButton;
+  switch (color) {
+    case 'gray':
+      themeButton = styles.gray;
+      break;
+    default:
+      themeButton = styles.primary;
+      break;
+  }
+  return <button className={`${styles.button} ${themeButton}`}>{children}</button>;
+};
 
 export default AnimatedButton;
