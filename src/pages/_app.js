6a51254cadf9d704e1a77cd6c0850d72e53cb2aa
Stanislav Drehval <stasdre@gmail.com>
2020-11-12 17:13:31 +0200

    stars animated background demo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,13 +4,18 @@ import '../assets/css/main.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="main">
-        <Component {...pageProps} />
+    <>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <div className="wrapper">
+        <Header />
+        <div className="main">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 }
 
