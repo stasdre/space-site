8293f6e098af846da6dfabd273b808c7c4aa4eda
Stanislav Drehval <stasdre@gmail.com>
2020-11-12 15:49:46 +0200

    fix custom-media css for css modules

--- src/components/CustomCheckbox/CustomCheckbox.js
+++ src/components/CustomCheckbox/CustomCheckbox.js
@@ -2,7 +2,7 @@ import styles from './CustomCheckbox.module.css';
 
 const CustomCheckbox = ({ label }) => (
   <label className={styles.checkbox}>
-    <input type="checkbox" />
+    <input className={styles.checkbox__input} type="checkbox" />
     <span className={styles.checkbox__name}>{label}</span>
   </label>
 );
