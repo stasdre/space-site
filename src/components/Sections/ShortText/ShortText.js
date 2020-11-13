08e6793295ef7055770301904e8d8aa00c9b9c82
Stanislav Drehval <stasdre@gmail.com>
2020-11-13 13:38:30 +0200

    create Section component

--- src/components/Sections/ShortText/ShortText.js
+++ src/components/Sections/ShortText/ShortText.js
@@ -1,12 +1,12 @@
 import { AnimatedButton } from '@/components/UI';
 import styles from './ShortText.module.css';
 
-const ShortText = ({ children, name }) => (
+const ShortText = ({ children, name, link }) => (
   <div className={styles.text}>
     <span className={styles.text__name}>{name}</span>
     {children}
     <div className={styles.text__button}>
-      <AnimatedButton>Подробнее</AnimatedButton>
+      <AnimatedButton link={link}>Подробнее</AnimatedButton>
     </div>
   </div>
 );
