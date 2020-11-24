fdb4a1e753b798aedf423171f32d5b01e1831c45
Stanislav Drehval <stasdre@gmail.com>
2020-11-24 11:21:55 +0200

    slider for reviews on mobile

--- src/components/Sections/Reviews/Reviews.js
+++ src/components/Sections/Reviews/Reviews.js
@@ -1,16 +1,40 @@
 import Image from 'next/image';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { AnimatedButton } from '@/components/UI';
 import styles from './Reviews.module.css';
 
 const Reviews = ({ reviews }) => {
   if (!reviews.length) return null;
 
+  const settings = {
+    dots: false,
+    infinite: true,
+    arrows: false,
+    centerMode: true,
+    dots: false,
+    lazyLoad: '/',
+    centerPadding: '10%',
+    responsive: [
+      {
+        breakpoint: 50000,
+        settings: 'unslick',
+      },
+      {
+        breakpoint: 768,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    ],
+  };
+
   return (
     <div className={styles.reviews}>
-      <ul className={styles.reviews__list}>
+      <Slider {...settings} className={styles.reviews__list}>
         {reviews.map((review) => {
           return (
-            <li key={review.id} className={styles.reviews__item}>
+            <div key={review.id} className={styles.reviews__item}>
               <a href="#" className={styles.review__link}>
                 <Image src={`/${review.img}`} width={582} height={397} />
               </a>
@@ -18,15 +42,34 @@ const Reviews = ({ reviews }) => {
                 <div className={styles.review__time}>{review.time}</div>
                 <div className={styles.review__title}>{review.title}</div>
               </div>
-            </li>
+            </div>
           );
         })}
-      </ul>
-      <div className={styles.review__button}>
-        <AnimatedButton>Смотреть все</AnimatedButton>
-      </div>
+      </Slider>
     </div>
   );
+  // return (
+  //   <div className={styles.reviews}>
+  //     <ul className={styles.reviews__list}>
+  //       {reviews.map((review) => {
+  //         return (
+  //           <li key={review.id} className={styles.reviews__item}>
+  //             <a href="#" className={styles.review__link}>
+  //               <Image src={`/${review.img}`} width={582} height={397} />
+  //             </a>
+  //             <div className={styles.review__info}>
+  //               <div className={styles.review__time}>{review.time}</div>
+  //               <div className={styles.review__title}>{review.title}</div>
+  //             </div>
+  //           </li>
+  //         );
+  //       })}
+  //     </ul>
+  //     <div className={styles.review__button}>
+  //       <AnimatedButton>Смотреть все</AnimatedButton>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default Reviews;
