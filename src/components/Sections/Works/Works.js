e42604b91eec31c1593d08362e48f7e7e5123245
Stanislav Drehval <stasdre@gmail.com>
2020-12-11 14:42:14 +0200

    works - slider on mobile devices

--- src/components/Sections/Works/Works.js
+++ src/components/Sections/Works/Works.js
@@ -1,21 +1,50 @@
+import Image from 'next/image';
 import { AnimatedButton } from '@/components/UI';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import styles from './Works.module.css';
+
+const settings = {
+  dots: false,
+  infinite: true,
+  arrows: false,
+  centerMode: true,
+  lazyLoad: '/',
+  centerPadding: '10%',
+  responsive: [
+    {
+      breakpoint: 50000,
+      settings: 'unslick',
+    },
+    {
+      breakpoint: 768,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    },
+  ],
+};
 
 const Works = ({ works }) => {
   if (!works.length) return null;
 
   return (
     <div className={styles.works}>
-      <ul className={styles.works__list}>
+      <Slider {...settings} className={styles.works__list}>
         {works.map((work) => (
-          <li className={styles.works__item} key={work.id}>
+          <div className={styles.works__item} key={work.id}>
             <a href="#" className={styles.works__link}>
-              <div className={styles.works__title}>{work.title}</div>
-              <span className={styles.works__year}>{work.year}</span>
+              <Image src={`/works/${work.img}`} width={582} height={723} />
+              <div className={styles.works__info}>
+                <div className={styles.works__top}>
+                  <div className={styles.works__title}>{work.type}</div>
+                </div>
+                <div className={styles.works__bottom}>{work.title}</div>
+              </div>
             </a>
-          </li>
+          </div>
         ))}
-      </ul>
+      </Slider>
       <div className={styles.works__button}>
         <AnimatedButton type="link" link="/works">
           Смотреть все
